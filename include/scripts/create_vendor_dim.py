e6a61b0cbf658a0b67279adf7ed3caf74d4506de
Bartosz Kędziorek <bartoszkedziorek2014@wp.pl>
2025-09-29 14:34:20 +0200

    add: loading data into 'end_date' and 'is_current' columns in DimVendor

--- include/scripts/create_vendor_dim.py
+++ include/scripts/create_vendor_dim.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StructField, StringType, StructType, IntegerType, DateType, BooleanType
-from pyspark.sql.functions import min
+from pyspark.sql.functions import min, max, col
 from pyspark.sql import functions as F
 import sys
 
@@ -56,6 +56,56 @@ properties = {
     "encrypt": "false"
 }
 table_name = "DimVendor"
+
+
+tmp_scd = final_scd_df
+final_scd_df = spark.createDataFrame([], schema=final_scd_schema)
+first_iteration = True
+
+while tmp_scd.count() != 0:
+    max_start_date = tmp_scd.groupBy("vendor_number").agg(max('start_date').alias('max_start_date'))
+
+    second_max_start_date = \
+        tmp_scd.join(max_start_date, 'vendor_number', how='inner') \
+            .where('start_date != max_start_date') \
+            .groupBy('vendor_number') \
+            .agg(max('start_date').alias('second_max_date'))
+    
+    if first_iteration == True:
+        add_to_scd = \
+            tmp_scd.join(max_start_date, on='vendor_number', how='inner') \
+            .where('max_start_date == start_date') \
+            .select(tmp_scd.vendor_number, tmp_scd.vendor_name, tmp_scd.start_date, tmp_scd.end_date, tmp_scd.is_current) \
+            .withColumn('is_current', F.lit(True))
+
+        final_scd_df = final_scd_df.union(add_to_scd)
+
+        first_iteration = False
+    
+
+    compare_dates_df = tmp_scd.join(
+        max_start_date, on='vendor_number', how='inner'
+    ).join(
+        second_max_start_date, on='vendor_number', how='inner'
+    ).where(
+        'start_date == second_max_date'
+    )
+
+    compare_dates_df = compare_dates_df.drop('end_date').withColumnRenamed('max_start_date', 'end_date')
+
+    final_scd_df = final_scd_df.union(
+        compare_dates_df.select(col('vendor_number'), col('vendor_name'), col('start_date'),
+                                col('end_date'), col('is_current'))
+    )
+
+    tmp_scd = tmp_scd.join(
+        max_start_date, on='vendor_number', how='inner'
+    ).where(
+        'start_date != max_start_date'
+    ).drop(
+        'max_start_date'
+    )
+
 
 final_scd_df = final_scd_df.withColumnsRenamed({
     'vendor_name': 'VendorName',
