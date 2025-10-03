d8b7bedff271ece1d08bf432b05e32957c8728b5
Bartosz Kędziorek <bartoszkedziorek2014@wp.pl>
2025-10-03 12:21:19 +0200

    fix: dag for VendorDim
    In previous commits task for creating VendorDim was using spark client mode. This commit
    makes this task use spark cluster mode

--- dags/update_vendor_dim.py
+++ dags/update_vendor_dim.py
@@ -8,10 +8,12 @@ from airflow.hooks.base import BaseHook
 from airflow.models.connection import Connection
 from pyspark.sql import functions as F
 
+
 @dag(dag_id='update_vendor_dim',
      start_date=datetime(2024, 1, 1),
      schedule="@daily"
      )
+
 def update_vendor_dim():
 
     conn: Connection =  BaseHook.get_connection('data_warehouse_presentation_layer')
@@ -32,68 +34,6 @@ def update_vendor_dim():
         jars='jars/sqljdbc_13.2/enu/jars/mssql-jdbc-13.2.0.jre11.jar'
     )
 
-
-    # @task.pyspark(conn_id='spark_cluster')
-    # def create_vendor_dim(spark: SparkSession):
-    #     tmp_scd: DataFrame = spark.read.parquet('ingest/raw_sales')
-
-    #     tmp_scd.createOrReplaceGlobalTempView('raw_sales')
-
-    #     final_scd_schema = StructType([
-    #         StructField('vendor_number', IntegerType(), False),
-    #         StructField('vendor_name', StringType(), False),
-    #         StructField('start_date', DateType(), False),
-    #         StructField('end_date', DateType(), True),
-    #         StructField('is_current', BooleanType(), True)
-    #     ])
-
-    #     final_scd_df = spark.createDataFrame([], schema=final_scd_schema)
-
-    #     tmp_scd = tmp_scd.groupBy(["vendor_number", "vendor_name"]).agg(min('date').alias('min_date1'))
-
-    #     while tmp_scd.count() != 0:
-            
-    #         merged_scd = tmp_scd.join(
-    #             other=tmp_scd.groupBy("vendor_number").agg(min('min_date1').alias('min_date2')),
-    #             on='vendor_number',
-    #             how='inner'
-    #         )
-
-    #         to_add = merged_scd.where('min_date1 = min_date2') 
-
-    #         final_scd_df = final_scd_df.union(
-    #             to_add.where('min_date1 = min_date2')
-    #             .drop('min_date2')
-    #             .withColumnRenamed('min_date1','start_date')
-    #             .withColumn('end_date', F.lit(None).cast(DateType()))
-    #             .withColumn('IsCurrent', F.lit(False))
-    #         )
-
-    #         merged_scd = merged_scd.where('min_date1 != min_date2')
-
-    #         tmp_scd = merged_scd.select(merged_scd.vendor_number, merged_scd.vendor_name, merged_scd.min_date1) \
-    #                             .withColumnRenamed('min_date1', 'date')
-
-    #     conn: Connection =  BaseHook.get_connection('data_warehouse_presentation_layer')
-    #     host = conn.host
-    #     database = conn.schema
-    #     username = conn.login
-    #     password = conn.password
-    #     port = conn.port
-
-    #     url = f"jdbc:sqlserver://{host}:{port};database={database}"
-    #     properties = {
-    #         "user": f"{username}",
-    #         "password": f"{password}",
-    #         "driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-    #     }
-
-    #     table_name = "DimVendor"
-    #     final_scd_df.write.jdbc(url=url, table=table_name, properties=properties)
-
-
     create_vendor_dim
-
-
 
 update_vendor_dim()
