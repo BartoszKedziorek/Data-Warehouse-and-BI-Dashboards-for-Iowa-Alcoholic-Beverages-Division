e651a953ebf30f165a1e7a0f45cdcc364476e959
Bartosz Kędziorek <bartoszkedziorek2014@wp.pl>
2025-10-06 09:34:07 +0200

    add: dag for creating DimPackaging

---
+++ dags/update_packaging_dim.py
@@ -0,0 +1,61 @@
+from airflow.sdk import dag, task
+from datetime import datetime
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.types import StructField, StringType, StructType, IntegerType, DateType, BooleanType
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from pyspark.sql.functions import min
+from airflow.hooks.base import BaseHook
+from airflow.models.connection import Connection
+from pyspark.sql import functions as F
+from google.cloud import bigquery
+import pyodbc
+from sqlalchemy import create_engine
+
+@dag(dag_id='update_packaging_dim',
+     start_date=datetime(2024, 1, 1),
+     schedule="@daily"
+     )
+def update_packaging_dim():
+
+    conn: Connection =  BaseHook.get_connection('data_warehouse_presentation_layer')
+    host = conn.host
+    database = conn.schema
+    username = conn.login
+    password = conn.password
+    port = conn.port
+
+    @task
+    def create_packaging_dim():
+    
+        client = bigquery.Client()
+
+        query = """
+            SELECT nr.pack as `NumberOfBottlesInPack`,
+                vol.bottle_volume_ml as `BottleVolumeML`
+            FROM (SELECT DISTINCT pack  FROM `bigquery-public-data.iowa_liquor_sales.sales`) as `nr`
+            CROSS JOIN
+            (SELECT DISTINCT bottle_volume_ml  FROM `bigquery-public-data.iowa_liquor_sales.sales`) as `vol`
+        """
+
+        query_job = client.query(query)
+
+        result = query_job.result()
+
+        df = result.to_dataframe()
+
+        connection_string = f'DRIVER={{/opt/microsoft/msodbcsql18/lib64/libmsodbcsql-18.5.so.1.1}};SERVER={host},{port};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes'
+
+        connection_url = f"mssql+pyodbc:///?odbc_connect={connection_string}"
+
+        engine = create_engine(connection_url, fast_executemany=True)
+
+        df.to_sql('DimPackaging', con=engine, schema='dbo', if_exists='append', index=False)
+
+
+    create_packaging_dim()
+
+
+update_packaging_dim()
+
+
+
