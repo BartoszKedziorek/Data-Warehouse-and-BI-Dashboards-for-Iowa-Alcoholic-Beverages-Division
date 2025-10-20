041353fc7d4f4f83c45628c76d0cfccb43e81289
Bartosz Kędziorek <bartoszkedziorek2014@wp.pl>
2025-10-20 08:32:37 +0200

    fix: remove hash value column from result dataframe in replace_hash_with_attributes function

--- include/scripts/modules/utils.py
+++ include/scripts/modules/utils.py
@@ -17,9 +17,11 @@ def replace_attributes_with_hash(df: DataFrame, attributes_cols: List[str]) -> T
 
 def replace_hash_with_attributes(df_with_hashed_attributes: DataFrame, hash_math_df: DataFrame) -> DataFrame:
     merge = df_with_hashed_attributes.alias('df').join(hash_math_df.alias('hd'), 'hashed_attributes_value', how='inner') \
-                                    .drop(F.col('df_hashed_attributes_value')) \
-                                    .drop(F.col('hd_hashed_attributes_value')) \
                                     .select(*([F.col(f'df.{col}').alias(f'{col}') for col in df_with_hashed_attributes.columns] +
                                              [F.col(f'hd.{col}').alias(f'{col}') for col in hash_math_df.columns]))
+    
+    cols_to_drop = [c for c in merge.columns if c.startswith('hashed_attributes_value')]
+
+    merge = merge.drop(*cols_to_drop)
     
     return merge
