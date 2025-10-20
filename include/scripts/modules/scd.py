ced388fb8333d290b79fbf743df2a5541e2cfa6d
Bartosz Kędziorek <bartoszkedziorek2014@wp.pl>
2025-10-20 08:44:18 +0200

    refactor: change algorithm in create_scd_from_input function to the more efficient one

--- include/scripts/modules/scd.py
+++ include/scripts/modules/scd.py
@@ -3,85 +3,65 @@ import pyspark.sql.functions as F
 from pyspark.sql.types import DateType, StructType
 from typing import List
 from copy import copy
-
+from scripts.modules.utils import replace_attributes_with_hash, replace_hash_with_attributes
+from pyspark.sql.window import Window
 
 def create_scd_from_input(spark: SparkSession, input_df: DataFrame, attributes_cols: List[str],
-                           date_col: str, natural_key_column: str, final_scd_schema: StructType) -> DataFrame: 
+                           date_col: str, natural_key_column: str, final_scd_schema: StructType) -> DataFrame:
+    
+    attributes_cols_without_natural_key = copy(attributes_cols)
+    attributes_cols_without_natural_key.remove(natural_key_column)
 
-    only_start_scd_df = spark.createDataFrame([], schema=final_scd_schema)
-    min_date_agg_df = input_df.groupBy(attributes_cols).agg(F.min(date_col).alias('min_date1'))
+    input_df_with_hashed_attributes, hash_match_df = replace_attributes_with_hash(input_df, attributes_cols_without_natural_key)
 
-    while min_date_agg_df.count() != 0:
-        
-        merged_scd = min_date_agg_df.join(
-            other=min_date_agg_df.groupBy(natural_key_column).agg(F.min('min_date1').alias('min_date2')),
-            on=natural_key_column,
-            how='inner'
-        )
-        to_add = merged_scd.where('min_date1 = min_date2') 
-        only_start_scd_df = only_start_scd_df.union(
-            to_add.where('min_date1 = min_date2')
-            .drop('min_date2')
-            .withColumnRenamed('min_date1','start_date')
-            .withColumn('end_date', F.lit(None).cast(DateType()))
-            .withColumn('is_current', F.lit(False))
-        )
-        merged_scd = merged_scd.where('min_date1 != min_date2')
+    min_date_agg_df = input_df_with_hashed_attributes.groupBy(natural_key_column, 'hashed_attributes_value') \
+                                                    .agg(F.min(date_col).alias('min_date1'))
 
-        select_cols = [merged_scd[col] for col in attributes_cols]
-        select_cols.append('min_date1')
+    windowSpec = Window.partitionBy(natural_key_column).orderBy(F.col("min_date1").asc())
 
-        min_date_agg_df = merged_scd.select(*select_cols)
-        
+    min_date_agg_df_with_seq_number_column = min_date_agg_df.withColumn('sequential_number', F.row_number().over(windowSpec)) # ponumerowanie rekordów dla
+        # każdego skelpu to jest tabela dla left join-a
 
-    final_scd_df = spark.createDataFrame([], schema=final_scd_schema)
-    first_iteration = True
+    min_date_agg_by_natural_key_df = input_df_with_hashed_attributes.groupBy(natural_key_column).agg(F.min(date_col).alias('min_date2'))
 
-    while only_start_scd_df.count() != 0:
-        max_start_date = only_start_scd_df.groupBy(natural_key_column).agg(F.max('start_date').alias('max_start_date'))
+    min_date_agg_by_nk_and_attribs_without_oldest_records = min_date_agg_df.alias('md').join(
+                                                            min_date_agg_by_natural_key_df.alias('mdnk'),
+                                                            on=natural_key_column,
+                                                            how='inner') \
+                                                            .where('md.min_date1 != mdnk.min_date2') \
+                                                            .select(
+                                                                F.col(f'md.{natural_key_column}').alias(natural_key_column),
+                                                                F.col(f'md.hashed_attributes_value').alias('hashed_attributes_value'),
+                                                                F.col(f'md.min_date1').alias('start_date')
+                                                            )
+    
+    windowSpec = Window.partitionBy(natural_key_column).orderBy(F.col("start_date").asc())
 
-        second_max_start_date = \
-            only_start_scd_df.join(max_start_date, natural_key_column, how='inner') \
-                .where('start_date != max_start_date') \
-                .groupBy(natural_key_column) \
-                .agg(F.max('start_date').alias('second_max_date'))
-        
-        if first_iteration == True:
-            add_to_scd = \
-                only_start_scd_df.join(max_start_date, on=natural_key_column, how='inner') \
-                .where('max_start_date == start_date') \
-                .select(*([only_start_scd_df[col] for col in attributes_cols] +
-                         [only_start_scd_df['start_date'], only_start_scd_df['end_date'], only_start_scd_df['is_current']])) \
-                .withColumn('is_current', F.lit(True))
+    min_date_agg_by_nk_and_attribs_without_oldest_records_with_seq_number = min_date_agg_by_nk_and_attribs_without_oldest_records \
+                                        .withColumn(
+                                            'sequential_number',
+                                            F.row_number().over(windowSpec)
+                                        )
 
-            final_scd_df = final_scd_df.union(add_to_scd)
+    raw_final_scd = min_date_agg_df_with_seq_number_column.alias('a').join(
+            min_date_agg_by_nk_and_attribs_without_oldest_records_with_seq_number.alias('b'),
+            (F.col(f'a.{natural_key_column}') == F.col(f'b.{natural_key_column}')) 
+            &
+            (F.col('a.sequential_number') == F.col('b.sequential_number')),
+            how='left'
+    ).select(
+        F.col(f'a.{natural_key_column}').alias(natural_key_column),
+        F.col(f'a.hashed_attributes_value').alias('hashed_attributes_value'),
+        F.col('a.min_date1').alias('start_date'),
+        F.col('b.start_date').alias('end_date')
+    )
 
-            first_iteration = False
-        
+    raw_final_scd = replace_hash_with_attributes(raw_final_scd, hash_match_df)
 
-        compare_dates_df = only_start_scd_df.join(
-            max_start_date, on=natural_key_column, how='inner'
-        ).join(
-            second_max_start_date, on=natural_key_column, how='inner'
-        ).where(
-            'start_date == second_max_date'
-        )
+    final_scd = raw_final_scd.withColumn('is_current', F.when(F.col('end_date').isNull(), True).otherwise(False))
 
-        compare_dates_df = compare_dates_df.drop('end_date').withColumnRenamed('max_start_date', 'end_date')
+    return final_scd # nieprzetestowane napisałem kod na brudno
 
-        final_scd_df = final_scd_df.union(
-            compare_dates_df.select(*([col for col in attributes_cols] + ['start_date', 'end_date', 'is_current']))
-        )
-
-        only_start_scd_df = only_start_scd_df.join(
-            max_start_date, on=natural_key_column, how='inner'
-        ).where(
-            'start_date != max_start_date'
-        ).drop(
-            'max_start_date'
-        )
-
-    return final_scd_df
 
 
 def get_oldest_records_from_scd(scd_df: DataFrame, attributes_cols_without_natural_key:str,
@@ -161,25 +141,44 @@ def merge_last_scd_record_with_scd_records_from_new_data_both_having_different_a
     return merge_for_insert_df
 
 
-# najstarszy rekord z nowego scd ma takie same wartości atrybutów jak najnowszy ze startego scd 
-def merge_last_scd_record_with_scd_records_from_new_data_both_having_same_attibutes(
-        spark: SparkSession, old_scd: DataFrame,
-          new_records: DataFrame, attributes_cols: List[str],
-          date_col: str, natural_key_column: str, final_scd_schema: StructType) -> DataFrame:
+# # najstarszy rekord z nowego scd ma takie same wartości atrybutów jak najnowszy ze startego scd 
+# def merge_last_scd_record_with_scd_records_from_new_data_both_having_same_attibutes(
+#         spark: SparkSession, old_scd: DataFrame,
+#           new_records: DataFrame, attributes_cols: List[str],
+#           date_col: str, natural_key_column: str, final_scd_schema: StructType) -> DataFrame:
     
-    new_scd = create_scd_from_input(spark, new_records, attributes_cols, date_col,
-                                     natural_key_column, final_scd_schema)
+#     new_scd = create_scd_from_input(spark, new_records, attributes_cols, date_col,
+#                                      natural_key_column, final_scd_schema)
     
-    attributes_cols_without_natural_key = copy(attributes_cols)
-    attributes_cols_without_natural_key.remove(natural_key_column)
+#     attributes_cols_without_natural_key = copy(attributes_cols)
+#     attributes_cols_without_natural_key.remove(natural_key_column)
 
-    oldest_records_from_new_scd = get_oldest_records_from_scd(new_scd, attributes_cols_without_natural_key, natural_key_column)
+#     oldest_records_from_new_scd = get_oldest_records_from_scd(new_scd, attributes_cols_without_natural_key, natural_key_column)
 
-    current_records_old_scd = old_scd.where("is_current == TRUE")
+#     current_records_old_scd = old_scd.where("is_current == TRUE")
 
-    
+#     # new_scd_for_merge = new_scd.exceptAll(oldest_records_from_new_scd)
 
-    
+#     new_scd_for_merge = new_scd.alias('nn').join(oldest_records_from_new_scd.alias('old'), on=natural_key_column, how='inner') \
+#                                 .where(F.col('nn.start_date') != F.col('old.start_date')) \
+#                                 .select(*([F.col(f"nn.{col}") for col in attributes_cols] + [F.col('nn.start_date'), F.col('nn.is_current'), F.col('nn.end_date')]))
 
+#     new_scd_for_merge.cache()
 
-    pass
+#     second_oldest_records_from_new_scd = get_oldest_records_from_scd(new_scd_for_merge, attributes_cols_without_natural_key, natural_key_column)
+
+#     merege_for_update_df = current_records_old_scd.join(second_oldest_records_from_new_scd, on=natural_key_column, how='inner')
+
+#     merege_for_update_df = merege_for_update_df.drop(current_records_old_scd['end_date']) \
+#                                                 .withColumn('end_date', second_oldest_records_from_new_scd['start_date']) \
+#                                                .drop(current_records_old_scd['is_current']) \
+#                                                .withColumn('is_current', F.lit(False)) \
+#                                                .select(*(['is_current', 'end_date', current_records_old_scd['start_date'], current_records_old_scd[natural_key_column]] +
+#                                                           [current_records_old_scd[col] for col in attributes_cols_without_natural_key]))
+#     merege_for_update_df.cache()
+
+#     merege_for_update_df = merege_for_update_df \
+#                         .select(sorted(merege_for_update_df.columns)) \
+#                         .union(new_scd_for_merge.select(sorted(new_scd_for_merge.columns)))
+
+#     return merege_for_update_df
--- include/tests/test_scd_functions.py
+++ include/tests/test_scd_functions.py
@@ -3,13 +3,16 @@ from pyspark.sql import SparkSession
 from scripts.modules.scd import create_scd_from_input, \
                                 merge_last_scd_record_with_oldest_scd_record_from_new_data_both_having_different_attibutes, \
                                 get_oldest_records_from_scd, \
-                                merge_last_scd_record_with_scd_records_from_new_data_both_having_different_attibutes
+                                merge_last_scd_record_with_scd_records_from_new_data_both_having_different_attibutes#, \
+                              #  merge_last_scd_record_with_scd_records_from_new_data_both_having_same_attibutes
 from pyspark.sql.types import StructType, StructField, DateType, IntegerType, StringType, BooleanType, Row
 import datetime
 import pytest_check as check
 import pyspark.sql.functions as F
 from typing import List
 from copy import copy
+import datetime
+
 
 class TestSCD:
     spark: SparkSession
@@ -19,6 +22,7 @@ class TestSCD:
         cls.spark = SparkSession.builder \
                         .appName('pytest-yarn-test') \
                         .config('spark.submit.deployMode', 'client') \
+                        .config('spark.yarn.am.memory','3G') \
                         .getOrCreate()
         
         cls.attributes_cols: List[str] = ['store_number', 'store_name', 'address',
@@ -46,7 +50,7 @@ class TestSCD:
 
 
     def test_create_scd_from_input_for_more_than_one_change(self):
-
+    
         input_df = self.spark.createDataFrame([
             Row(invoice_and_item_number='RINV-04934100006', date=datetime.date(2024, 1, 1),
                 store_number=4970, store_name='JEFF\'S MARKET / WEST LIBERTY',
@@ -117,6 +121,8 @@ class TestSCD:
 
         df = create_scd_from_input(self.spark, input_df, self.attributes_cols,
                                     'date', 'store_number', self.final_scd_schema)
+        df.count()
+        df.cache()
 
         first_scd_record = df.where(F.col('start_date') == F.to_date(F.lit('2024-1-1'))).collect()[0]
         second_scd_record = df.where(F.col('start_date') == F.to_date(F.lit('2024-1-12'))).collect()[0]
@@ -347,5 +353,109 @@ class TestSCD:
         check.equal(fifth_record['is_current'], True)
 
 
+    # def test_merge_last_scd_record_with_scd_records_from_new_data_both_having_same_attibutes(self):
+    #     old_scd = self.spark.createDataFrame([
+    #         Row(store_number=2502, store_name='HY-VEE WINE AND SPIRITS (1022) / ANKENY',
+    #             address='410 NORTH ANKENY BLVD', city='ANKENY',
+    #             zip_code=50021, store_location='POINT(-93.602561976 41.73460601)',
+    #             start_date=datetime.date(2024, 11, 12),end_date=datetime.date(2024, 11, 27), is_current=False),
+    #         Row(store_number=2502, store_name='HY-VEE WINE AND SPIRITS (1022) / ANKENY CHANGE 1',
+    #             address='410 NORTH ANKENY BLVD', city='ANKENY',
+    #             zip_code=50021, store_location='POINT(-93.602561976 41.73460601)',
+    #             start_date=datetime.date(2024, 11, 27), end_date=None, is_current=True)
+    #     ])
 
+    #     new_records = self.spark.createDataFrame([
+    #         Row(invoice_and_item_number='RINV-04934100010', date=datetime.date(2025, 1, 11),
+    #             store_number=2502, store_name='HY-VEE WINE AND SPIRITS (1022) / ANKENY CHANGE 1',
+    #             address='410 NORTH ANKENY BLVD', city='ANKENY',
+    #             zip_code=50021, store_location='POINT(-93.602561976 41.73460601)',
+    #             county_number=23, county='POLK',
+    #             category=1032100, category_name='IMPORTED VODKAS',
+    #             vendor_number=370, vendor_name='PERNOD RICARD USA',
+    #             item_number=34007, item_description='ABSOLUT SWEDISH VODKA 80PRF',
+    #             pack=12, bottle_volume_ml=1000,
+    #             state_bottle_cost='14.99', state_bottle_retail='22.49',
+    #             bottles_sold=12, sale_dollars='539.76',
+    #             volume_sold_liters='-24.0', volume_sold_gallons='-6.34'
+    #             ),
+    #         Row(invoice_and_item_number='RINV-04934100010', date=datetime.date(2025, 1, 12),
+    #             store_number=2502, store_name='HY-VEE WINE AND SPIRITS (1022) / ANKENY CHANGE 1',
+    #             address='410 NORTH ANKENY BLVD', city='ANKENY',
+    #             zip_code=50021, store_location='POINT(-93.602561976 41.73460601)',
+    #             county_number=23, county='POLK',
+    #             category=1032100, category_name='IMPORTED VODKAS',
+    #             vendor_number=370, vendor_name='PERNOD RICARD USA',
+    #             item_number=34007, item_description='ABSOLUT SWEDISH VODKA 80PRF',
+    #             pack=12, bottle_volume_ml=1000,
+    #             state_bottle_cost='14.99', state_bottle_retail='22.49',
+    #             bottles_sold=12, sale_dollars='539.76',
+    #             volume_sold_liters='-24.0', volume_sold_gallons='-6.34'
+    #             ),
+    #         Row(invoice_and_item_number='RINV-04934100010', date=datetime.date(2025, 1, 15),
+    #             store_number=2502, store_name='HY-VEE WINE AND SPIRITS (1022) / ANKENY CHANGE 2',
+    #             address='410 NORTH ANKENY BLVD', city='ANKENY',
+    #             zip_code=50021, store_location='POINT(-93.602561976 41.73460601)',
+    #             county_number=23, county='POLK',
+    #             category=1032100, category_name='IMPORTED VODKAS',
+    #             vendor_number=370, vendor_name='PERNOD RICARD USA',
+    #             item_number=34007, item_description='ABSOLUT SWEDISH VODKA 80PRF',
+    #             pack=12, bottle_volume_ml=1000,
+    #             state_bottle_cost='14.99', state_bottle_retail='22.49',
+    #             bottles_sold=12, sale_dollars='539.76',
+    #             volume_sold_liters='-24.0', volume_sold_gallons='-6.34'
+    #             ),
+    #         Row(invoice_and_item_number='RINV-04934100010', date=datetime.date(2025, 1, 17),
+    #             store_number=2502, store_name='HY-VEE WINE AND SPIRITS (1022) / ANKENY CHANGE 3',
+    #             address='410 NORTH ANKENY BLVD', city='ANKENY',
+    #             zip_code=50021, store_location='POINT(-93.602561976 41.73460601)',
+    #             county_number=23, county='POLK',
+    #             category=1032100, category_name='IMPORTED VODKAS',
+    #             vendor_number=370, vendor_name='PERNOD RICARD USA',
+    #             item_number=34007, item_description='ABSOLUT SWEDISH VODKA 80PRF',
+    #             pack=12, bottle_volume_ml=1000,
+    #             state_bottle_cost='14.99', state_bottle_retail='22.49',
+    #             bottles_sold=12, sale_dollars='539.76',
+    #             volume_sold_liters='-24.0', volume_sold_gallons='-6.34'
+    #             )
+    #     ])
+
+    #     merged_scd = merge_last_scd_record_with_scd_records_from_new_data_both_having_same_attibutes(
+    #         self.spark,old_scd, new_records, self.attributes_cols, 'date', 'store_number', self.final_scd_schema)
+        
+
+    #     first_scd_record = merged_scd.where(F.col('start_date') == F.to_date(F.lit('2024-11-27'))).collect()[0]
+    #     second_scd_record = merged_scd.where(F.col('start_date') == F.to_date(F.lit('2025-1-15)'))).collect()[0]
+    #     third_scd_record = merged_scd.where(F.col('start_date') == F.lit('2025-1-17')).collect()[0]
+
+
+    #     merged_scd.explain()
+
+    #     check.equal(first_scd_record['store_number'], 2502)
+    #     check.equal(first_scd_record['store_name'], 'HY-VEE WINE AND SPIRITS (1022) / ANKENY CHANGE 1')
+    #     check.equal(first_scd_record['zip_code'], 50021)
+    #     check.equal(first_scd_record['store_location'], 'POINT(-93.602561976 41.73460601)')
+    #     check.equal(first_scd_record['address'], '410 NORTH ANKENY BLVD')
+    #     check.equal(first_scd_record['start_date'], datetime.date(2024, 11, 27))
+    #     check.equal(first_scd_record['end_date'], datetime.date(2025, 1, 15))
+    #     check.equal(first_scd_record['is_current'], False)
+        
+    #     check.equal(second_scd_record['store_number'], 2502)
+    #     check.equal(second_scd_record['store_name'], 'HY-VEE WINE AND SPIRITS (1022) / ANKENY CHANGE 2')
+    #     check.equal(second_scd_record['zip_code'], 50021)
+    #     check.equal(second_scd_record['store_location'], 'POINT(-93.602561976 41.73460601)')
+    #     check.equal(second_scd_record['address'], '410 NORTH ANKENY BLVD')
+    #     check.equal(second_scd_record['start_date'], datetime.date(2025, 1, 15))
+    #     check.equal(second_scd_record['end_date'], datetime.date(2025, 1, 17))
+    #     check.equal(second_scd_record['is_current'], False)
+
+    #     check.equal(third_scd_record['store_number'], 2502)
+    #     check.equal(third_scd_record['store_name'], 'HY-VEE WINE AND SPIRITS (1022) / ANKENY CHANGE 3')
+    #     check.equal(third_scd_record['zip_code'], 50021)
+    #     check.equal(third_scd_record['store_location'], 'POINT(-93.602561976 41.73460601)')
+    #     check.equal(third_scd_record['address'], '410 NORTH ANKENY BLVD')
+    #     check.equal(third_scd_record['start_date'], datetime.date(2025, 1, 17))
+    #     check.equal(third_scd_record['end_date'], None)
+    #     check.equal(third_scd_record['is_current'], True)      
+        
 
